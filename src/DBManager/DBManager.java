43689961f9685590cd4525350d871655997c7a09
knyazov <tantrumak@gmail.com>
2022-06-04 19:25:32 +0600

    started news portal

---
+++ src/DBManager/DBManager.java
@@ -0,0 +1,19 @@
+package DBManager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBManager {
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee_db?useUnicode=tru&serverTimezone=UTC", "root", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
