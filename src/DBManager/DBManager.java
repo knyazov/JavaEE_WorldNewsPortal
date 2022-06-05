d89bb5ba97661850486f5910af156e2e049754e6
knyazov <tantrumak@gmail.com>
2022-06-05 18:47:26 +0600

    started news portal

--- src/DBManager/DBManager.java
+++ src/DBManager/DBManager.java
@@ -1,7 +1,12 @@
 package DBManager;
 
+import Entities.News;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class DBManager {
 
@@ -14,5 +19,32 @@ public class DBManager {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    public static ArrayList<News> getAllNews() {
+        ArrayList<News> allNews = new ArrayList<>();
+
+        try {
+            PreparedStatement statement = connection.prepareStatement("" +
+                    "SELECT * FROM news" +
+                    "");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                News news = new News();
+                news.setId(resultSet.getLong("id"));
+                news.setTitle(resultSet.getString("title"));
+                news.setShortContent(resultSet.getString("short_content"));
+                news.setContent(resultSet.getString("content"));
+                news.setPostDate(resultSet.getTimestamp("post_date"));
+                news.setPictureUrl(resultSet.getString("picture_url"));
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return allNews;
     }
 }
