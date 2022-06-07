918ed4e5300c54f76264119f436930c5026f0683
knyazov <tantrumak@gmail.com>
2022-06-07 13:21:14 +0600

    started news portal

--- src/DBManager/DBManager.java
+++ src/DBManager/DBManager.java
@@ -114,7 +114,7 @@ public class DBManager {
                     "SELECT * FROM publications" +
                     "");
             ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 Publications publication = new Publications();
                 publication.setId(resultSet.getLong("id"));
                 publication.setName(resultSet.getString("name"));
@@ -155,5 +155,45 @@ public class DBManager {
 
         return languages;
     }
+
+    public static News getNewsById(Long id) {
+        News news = null;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement("" +
+                    "SELECT n.id, n.title, n.short_content, n.content, n.post_date, n.picture_url, n.language_id, n.publication_id, p.name as publication_name, l.code " +
+                    "FROM (news n INNER JOIN languages l on l.id = n.language_id) " +
+                    "INNER JOIN publications p " +
+                    "ON p.id = n.publication_id" +
+                    "WHERE language_id = ? ");
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                news = new News();
+                news.setId(resultSet.getLong("id"));
+                news.setTitle(resultSet.getString("title"));
+                news.setShortContent(resultSet.getString("short_content"));
+                news.setContent(resultSet.getString("content"));
+                news.setPostDate(resultSet.getTimestamp("post_date"));
+                news.setPictureUrl(resultSet.getString("picture_url"));
+
+                Publications publications = new Publications();
+                publications.setId(resultSet.getLong("publication_id"));
+                publications.setName(resultSet.getString("publication_name"));
+
+                Languages languages = new Languages();
+                languages.setId(resultSet.getLong("language_id"));
+                languages.setCode(resultSet.getString("code"));
+
+                news.setPublications(publications);
+                news.setLanguages(languages);
+            }
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return news;
+    }
+
 
 }
